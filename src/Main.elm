7bb3768672afc14456d9e4b427ea85fe6a03f235
Andy Park <atpark333@gmail.com>
2020-05-06 13:16:55 +0900

    Add input fields

--- src/Main.elm
+++ src/Main.elm
@@ -1,39 +1,46 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Html exposing (Html, button, div, input, text, label)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
 
 -- MAIN
 main =
-    Browser.sandbox { init = init, update = update, view = view }
+  Browser.sandbox { init = init, update = update, view = view }
 
 -- MODEL
-type alias Model = Int
+type alias Model =
+  { name: String
+    , date: String 
+  }
 
 init : Model
 init =
-    0
+  { name = "",
+    date = "" }
 
 -- UPDATE
 type Msg
-    = Increment
-    | Decrement
+  = NameChange String
+  | DateChange String
 
 update : Msg -> Model -> Model
 update msg model =
-    case msg of
-        Increment ->
-            model + 1
-        Decrement ->
-            model - 1
+  case msg of
+    NameChange newName ->
+      { model | name = newName }
+    DateChange newDate ->
+      { model | date = newDate }
 
 
 -- VIEW
 view : Model -> Html Msg
 view model =
-    div []
-        [ button [ onClick Decrement ] [ text "-" ]
-        , div [] [ text (String.fromInt model) ]
-        , button [ onClick Increment ] [ text "+" ]
-        ]
+  div []
+    [ label [ for "name-input" ] [ text "Event" ]
+    , input [ id "name-input", placeholder "Event Name", value model.name, onInput NameChange, class "form-control" ] []
+    , label [ for "date-input" ] [ text "Date" ]
+    , input [ id "date-input", type_ "date", value model.date, onInput DateChange, class "form-control" ] []
+    , button [] [ text "Submit" ]
+    ]
